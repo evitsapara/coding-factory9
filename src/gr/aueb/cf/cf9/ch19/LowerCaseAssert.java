0f7edabe74bc75084d813b91e1cc3ffa64e71adf
evitsapara <evi.tsapi@gmail.com>
2026-01-13 19:13:59 +0200

    feat: add lower case assert

---
+++ src/gr/aueb/cf/cf9/ch19/LowerCaseAssert.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.cf9.ch19;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LowerCaseAssert {
+
+    public static void main(String[] args) {
+        String s = "aaafffffcccvbb";        // at least 8 characters, at least one of them lower case
+
+        Pattern pattern = Pattern.compile("(?=.*[a-z])^.{8,}$");    // Zero-length assertion
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
